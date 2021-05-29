48cde0b66d828135f9bc6c9bdb042a3f8b8d3d44
shivamminus <dubeyshivam044@gmail.com>
2021-05-29 20:06:48 +0530

    added logger and swagger

--- src/main/java/com/eureka/demo/EurekaServerAppApplication.java
+++ src/main/java/com/eureka/demo/EurekaServerAppApplication.java
@@ -1,15 +1,25 @@
 package com.eureka.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerAppApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(EurekaServerAppApplication.class);
+
 
 	public static void main(String[] args) {
+		
+        System.setProperty("server.connection-timeout","120000");
+        
 		SpringApplication.run(EurekaServerAppApplication.class, args);
+		logger.info("EUREKA SERVER STARTED...");
 	}
 
 }
